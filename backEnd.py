7fbee388798cda4697cbcbe8e8a6946ad7182aec
MRizkyF_ <firmansyah.mrf@gmail.com>
2021-10-04 10:52:15 +0700

    mencoba menambahkan data ke database

--- backEnd.py
+++ backEnd.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import cursor
 
 db = mysql.connector.connect(
     host="localhost",
@@ -9,3 +10,11 @@ db = mysql.connector.connect(
 
 if db.is_connected():
   print("Berhasil terhubung ke database")
+
+dbCursor = db.cursor()
+sql = "INSERT INTO user (namaLengkap, username, kataSandi) VALUES (%s, %s, %s)"
+val = ("Iman", "admin", "123")
+dbCursor.execute(sql, val)
+
+db.commit()
+print("{} Data ditambahkan".format(dbCursor.rowcount))
